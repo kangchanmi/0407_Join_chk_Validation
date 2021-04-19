d3eec174b37d9a3fa53b1d0bb1a0d134abebf281
kangchanmi <alslrkdcks@naver.com>
2021-04-19 17:47:06 +0900

    0407_체크박스 유효성 검사_jQuery 적용_수정

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+/* eslinet-visible */
+
 $(function () {
 
     $("#allAgreeChk").click(function () {
@@ -11,26 +13,24 @@ $(function () {
             $(".check").prop("checked", false);
         }
         /* 전체동의 버튼 선택 / 해제 */
-
-
-        $(".check").click(function () {
-
-            var chk1 = $("#chk1").prop("checked");
-            var chk2 = $("#chk2").prop("checked");
-            var chk3 = $("#chk3").prop("checked");
-            var chk4 = $("#chk4").prop("checked");
-
-            if (chk1 && chk2 && chk3 && chk4 == true) {
-                $("#allAgreeChk").prop("checked", "true");
-            } else {
-                $("#allAgreeChk").prop("checked", false);
-            }
-
-
-        });
     });
 
 
+    $(".check").click(function () {
+
+        var chk1 = $("#chk1").prop("checked");
+        var chk2 = $("#chk2").prop("checked");
+        var chk3 = $("#chk3").prop("checked");
+        var chk4 = $("#chk4").prop("checked");
+
+        if (chk1 && chk2 && chk3 && chk4 == true) {
+            $("#allAgreeChk").prop("checked", true);
+        } else {
+            $("#allAgreeChk").prop("checked", false);
+        }
+
+
+    });
 
 
 
